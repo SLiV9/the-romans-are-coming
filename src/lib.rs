27d24cfa1ffd7bc5d50ddaf4a2135bd7da7e4b59
Sander in 't Veld <sanderintveld@hotmail.com>
2022-08-17 10:42:11 +0200

    Fixed rustfmt not going off on lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -26,82 +26,82 @@ const QUICK_TEST: bool = true;
 
 enum Game
 {
-    Loading,
-    Menu(Menu),
-    Level(Level),
+	Loading,
+	Menu(Menu),
+	Level(Level),
 }
 
 enum Progress
 {
-    Menu,
-    Level(level::Transition),
+	Menu,
+	Level(level::Transition),
 }
 
 #[no_mangle]
 fn update()
 {
-    let game = GAME.get_mut();
-    let transition = match game
-    {
-        Game::Loading =>
-        {
-            setup();
-            if QUICK_TEST
-            {
-                Some(Progress::Level(level::Transition { rng_seed: 0 }))
-            }
-            else
-            {
-                Some(Progress::Menu)
-            }
-        }
-        Game::Menu(menu) =>
-        {
-            let transition = menu.update();
-            match transition
-            {
-                Some(menu::Transition::Start{rng_seed}) =>
-                {
-                    let data = level::Transition { rng_seed };
-                    Some(Progress::Level(data))
-                }
-                None => None
-            }
-        }
-        Game::Level(level) =>
-        {
-            let transition = level.update();
-            match transition
-            {
-                Some(data) => Some(Progress::Level(data)),
-                None => None,
-            }
-        }
-    };
-    match transition
-    {
-        Some(Progress::Menu) =>
-        {
-            let menu = Menu::new();
-            *game = Game::Menu(menu);
-        }
-        Some(Progress::Level(data)) =>
-        {
-            let level = Level::new(data.rng_seed);
-            *game = Game::Level(level);
-        }
-        None => (),
-    }
+	let game = GAME.get_mut();
+	let transition = match game
+	{
+		Game::Loading =>
+		{
+			setup();
+			if QUICK_TEST
+			{
+				Some(Progress::Level(level::Transition { rng_seed: 0 }))
+			}
+			else
+			{
+				Some(Progress::Menu)
+			}
+		}
+		Game::Menu(menu) =>
+		{
+			let transition = menu.update();
+			match transition
+			{
+				Some(menu::Transition::Start { rng_seed }) =>
+				{
+					let data = level::Transition { rng_seed };
+					Some(Progress::Level(data))
+				}
+				None => None,
+			}
+		}
+		Game::Level(level) =>
+		{
+			let transition = level.update();
+			match transition
+			{
+				Some(data) => Some(Progress::Level(data)),
+				None => None,
+			}
+		}
+	};
+	match transition
+	{
+		Some(Progress::Menu) =>
+		{
+			let menu = Menu::new();
+			*game = Game::Menu(menu);
+		}
+		Some(Progress::Level(data)) =>
+		{
+			let level = Level::new(data.rng_seed);
+			*game = Game::Level(level);
+		}
+		None => (),
+	}
 
-    match game
-    {
-        Game::Loading => (),
-        Game::Menu(menu) => menu.draw(),
-        Game::Level(level) => level.draw(),
-    }
+	match game
+	{
+		Game::Loading => (),
+		Game::Menu(menu) => menu.draw(),
+		Game::Level(level) => level.draw(),
+	}
 }
 
 fn setup()
 {
-    palette::setup();
+	palette::setup();
 }
--- src/menu.rs
+++ src/menu.rs
@@ -8,62 +8,81 @@ use crate::wasm4::*;
 
 use crate::palette;
 
-pub struct Menu {
+pub struct Menu
+{
 	ticks: u32,
 }
 
 const NUM_INTRO_ANIMATION_TICKS: u32 = 160;
 
-impl Menu {
-	pub fn new() -> Self {
+impl Menu
+{
+	pub fn new() -> Self
+	{
 		Self { ticks: 0 }
 	}
 
-	pub fn update(&mut self) -> Option<Transition> {
+	pub fn update(&mut self) -> Option<Transition>
+	{
 		let gamepad = unsafe { *GAMEPAD1 };
 		let mousebuttons = unsafe { *MOUSE_BUTTONS };
 
 		self.ticks += 1;
 
-		if gamepad & BUTTON_1 != 0 {
+		if gamepad & BUTTON_1 != 0
+		{
 			Some(Transition::Start {
 				rng_seed: self.ticks as u64,
 			})
-		} else if mousebuttons & MOUSE_LEFT != 0 {
+		}
+		else if mousebuttons & MOUSE_LEFT != 0
+		{
 			Some(Transition::Start {
 				rng_seed: self.ticks as u64,
 			})
-		} else {
+		}
+		else
+		{
 			None
 		}
 	}
 
-	pub fn draw(&mut self) {
+	pub fn draw(&mut self)
+	{
 		unsafe { *PALETTE = palette::DEFAULT };
 
 		unsafe { *DRAW_COLORS = 3 };
 		{
-			if self.ticks > 60 {
+			if self.ticks > 60
+			{
 				text("THE", 60, 20);
 			}
-			if self.ticks > 80 {
+			if self.ticks > 80
+			{
 				text("ROMANS", 60 + 8, 30);
 			}
-			if self.ticks > 100 {
+			if self.ticks > 100
+			{
 				text("ARE", 60 + 16, 40);
 			}
-			if self.ticks > 120 {
+			if self.ticks > 120
+			{
 				text("COMING!", 60 + 24, 50);
 			}
 		}
 
-		if self.ticks > NUM_INTRO_ANIMATION_TICKS + 30 {
+		if self.ticks > NUM_INTRO_ANIMATION_TICKS + 30
+		{
 			unsafe { *DRAW_COLORS = 2 }
 			text("Click to begin", 3, 150);
 		}
 	}
 }
 
-pub enum Transition {
-	Start { rng_seed: u64 },
+pub enum Transition
+{
+	Start
+	{
+		rng_seed: u64
+	},
 }
